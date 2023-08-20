549250b8ef2b02db226cbbe111b328cb35fd945b
edilson gomes de lima <edskateicevida@gmail.com>
2023-08-20 14:45:54 -0300

    Estrutura de condição if, else,ele, if

---
+++ app.js
@@ -0,0 +1,15 @@
+const idade = 16;
+ if (idade < 13) {
+    console.log("Criança");
+
+ } else if(idade < 20) {
+console.log("Adolecente");
+ } else {
+    console.log("Adulto");
+ }
+
+ if(false) {
+    console.log("Isso executa");
+ } else {
+    console.log("Isso agora é executado!");
+ }
