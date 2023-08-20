009e1797da363486e7a1432483a85d2cbdb0d7b6
edilson gomes de lima <edskateicevida@gmail.com>
2023-08-20 15:13:35 -0300

    listas de frutas

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const idade = 16;
- if (idade < 13) {
-    console.log("Criança");
+if (idade < 13) {
+  console.log("Criança");
+} else if (idade < 20) {
+  console.log("Adolecente");
+} else {
+  console.log("Adulto");
+}
 
- } else if(idade < 20) {
-console.log("Adolecente");
- } else {
-    console.log("Adulto");
- }
+if (false) {
+  console.log("Isso executa");
+} else {
+  console.log("Isso agora é executado!");
+}
 
- if(false) {
-    console.log("Isso executa");
- } else {
-    console.log("Isso agora é executado!");
- }
+// Switch
+
+const fruta = "Banana";
+
+switch (fruta) {
+  case "Banana":
+    console.log("Banana é fruta!");
+  case "Maçã":
+    console.log("Maçã é fruta!");
+}
